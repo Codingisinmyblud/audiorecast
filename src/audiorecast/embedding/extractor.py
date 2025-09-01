Corrected embedding dimension from time-averaged to feature-averaged
Changed dim=-1 to dim=1 in Wav2Vec2 wrapper to extract proper 768-D embeddings for audio matching instead of time-step averages.

src/audiorecast/embedding/extractor.py
@@ -21,5 +21,5 @@ class EmbeddingExtractor:
 
         wav = wav.to(self.device)
         reps, _ = self.model.extract_features(wav)  # list of layers
-        vec = reps[-1].mean(dim=-1).squeeze(0)      # (768, _)
+        vec = reps[-1].mean(dim=1).squeeze(0)      # (768, _)
         return vec.cpu()