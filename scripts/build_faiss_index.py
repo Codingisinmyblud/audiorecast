aefcb9fb939904eb2bcaab53c021e6cd210a7224
Qasim Khawaja <117865858+Codingisinmyblud@users.noreply.github.com>
2025-09-01 17:32:37 -0700

    Updated comments in build_faiss_index.py

--- scripts/build_faiss_index.py
+++ scripts/build_faiss_index.py
@@ -1,4 +1,5 @@
 # for reference: audiorecast/scripts/build_faiss_index.py
+# to run: poetry run python audiorecast/scripts/build_faiss_index.py
 
 from audiorecast.matching.index_builder import run
 
